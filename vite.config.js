6e028cf72a963f162795005e095365bb6f5daaf8
shaharearkabir256-del <shaharearkabir256@gmail.com>
2025-07-30 06:51:33 +0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// GitHub Pages এ repo path সেট করা
+export default defineConfig({
+  plugins: [react()],
+  base: '/dwellwell-buildshare/', // তোমার repo নাম
+})
